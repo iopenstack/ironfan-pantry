245a30c5292586f3beeb8f06fcf08fd1217269c6
Bart Vercammen <bart.vercammen@portico.io>
2013-04-24 10:48:01 +0200

    add installation of storm runtime for deployment of topologies + small corrections

--- cookbooks/storm/metadata.rb
+++ cookbooks/storm/metadata.rb
@@ -3,8 +3,8 @@ maintainer       "Technicolor, Portico"
 maintainer_email "dev@portico.io"
 license          "Apache 2.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
 
-version          "0.0.2"
 description      "Installs Twitter's Storm distributed computation system"
 
 depends          "java"
