e0d17b14aff86f33937f7625dee7ffc2c99f18ab
Gert Verbruggen <mushimo@gmail.com>
2013-07-08 10:12:50 +0200

    Temporarily turned link creation in ganglia collector off and back on to be able to kick oregon

--- cookbooks/ganglia/recipes/collector.rb
+++ cookbooks/ganglia/recipes/collector.rb
@@ -38,25 +38,25 @@ kill_old_service('ganglia-monitor'){ pattern 'gmond' }
 
 standard_dirs('ganglia') do
     directories [:home_dir, :log_dir, :conf_dir, :pid_dir]
-    user        node[:ganglia][:user]
-    group       node[:ganglia][:group]
+    user  node[:ganglia][:user]
+    group node[:ganglia][:group]
 end
 
 volume_dirs('ganglia.data') do
-    selects     :single
-    owner       node[:ganglia][:user]
-    group       node[:ganglia][:group]
-    path        'ganglia/data/rrds'
+    selects :single
+    owner node[:ganglia][:user]
+    group node[:ganglia][:group]
+    path  'ganglia/data/rrds'
 end
 
 link "#{node[:ganglia][:home_dir]}/rrds" do
-    to      node[:ganglia][:data_dir]
+    to node[:ganglia][:data_dir]
 end
 
 # global data storage for ganglia (gmetad)
 runit_service "ganglia_metad" do
-    run_state       :nothing
-    options         ({ 
+    run_state :nothing
+    options({
         :dirs => {
             :pid    => node[:ganglia][:pid_dir],
             :conf   => node[:ganglia][:conf_dir],
@@ -71,4 +71,3 @@ find_all_monitorable_clusters.each do |cluster|
     Chef::Log.info("Ganglia::collector --- found a cluster to monitor: #{cluster}")
     collector_service(cluster)
 end
-
