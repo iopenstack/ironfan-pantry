984bfff5c145b8173bd0bfd961f8d3a623655668
Jean-Charles Nade <jeancharles.nade@technicolor.com>
2013-07-24 17:09:30 +0200

    receipes for ganglia backup, with new params

--- cookbooks/ganglia/recipes/backup.rb
+++ cookbooks/ganglia/recipes/backup.rb
@@ -1,9 +1,10 @@
+
 include_recipe "github"
 include_recipe "cron"
 
 
 
-git "/mnt/hecube" do
+git "/mnt/hecube3" do
     repo        "git@github.com:Technicolor-Portico/hecube.git"
     revision    "master"
     user        user
@@ -14,10 +15,12 @@ end
 
 
 config = {
-    :region => node[:ganglia][:grid]
+    :region => node[:ganglia][:grid],
+    :path => node[:ganglia][:home_dir]
+
 }
 
-template "/mnt/hecube/ganglia/servername.txt" do
+template "/mnt/hecube3/ganglia/servername.txt" do
     source      'hecube-servername.erb'
     backup      false
     owner       node[:ganglia][:user]
@@ -30,7 +33,7 @@ end
 cron "ganglia_backup" do
   minute 0
   user user
-  command "/mnt/hecube/ganglia/cron.sh >> /var/log/ganglia_backup.log 2>&1"
+  command "/mnt/hecube3/ganglia/ganglia_backup.sh -d #{node[:ganglia][:home_dir]}  -p #{node[:ganglia][:grid].split("_").first} -c #{node[:ganglia][:grid].split("_").last}  >> /var/log/ganglia_backup.log 2>&1"
   action :create
 end
 
